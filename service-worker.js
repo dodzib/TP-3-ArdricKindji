51dd03fb9bbb0c9c0242c5e3ade472bd5d13acaf
Ardric KINDJI <ardric85@gmail.com>
2024-07-25 18:00:17 -0400

    Correction worker

--- service-worker.js
+++ service-worker.js
@@ -63,7 +63,7 @@ self.addEventListener('fetch', (evt) => {
                 .catch(() => {
                     return caches.open(CACHE_NAME)
                     .then((cache) => {
-             return cache.match('offline.html' );
+             return cache.match('offline.html');
     });
     })
     );
